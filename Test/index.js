ca5d6e81e2aa5e44a988b32429e442e01995f7c2
vegetKa <vegetka@gmail.com>
2018-07-21 15:12:18 +0100

    socket

--- Test/index.js
+++ Test/index.js
@@ -7,12 +7,17 @@ var io = require('socket.io')(server);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('connection', function(socket){
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+io.on('connection', function(socket){
     console.log('connection to client estabilished');
     socket.on('disconnect', function(){
         console.log('Server has disconnected');
     });
 });
+
 server.listen(3000, function(){
     console.log('Listening on port 3000...');
 });
