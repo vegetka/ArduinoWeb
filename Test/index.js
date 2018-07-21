fec8ba26621277b2f2540ed5eec734c404ded50e
vegetKa <vegetka@gmail.com>
2018-07-21 14:53:26 +0100

    start

--- Test/index.js
+++ Test/index.js
@@ -2,18 +2,17 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var server = http.createServer(app);
+var io = require('socket.io')(server);
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
-var title = "Some words starting with p";
-var componentArray = ['potentiometer', 'piezo', 'photoresistor', 'pushbutton'];
-app.get('/', function(req, res){
-    res.render('index', {
-        title: title,
-        components: componentArray
+app.get('connection', function(socket){
+    console.log('connection to client estabilished');
+    socket.on('disconnect', function(){
+        console.log('Server has disconnected');
     });
 });
-
 server.listen(3000, function(){
     console.log('Listening on port 3000...');
 });
