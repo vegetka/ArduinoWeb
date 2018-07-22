49e0a16fa5388a5e96a391b487e49de6f6e325f7
vegetKa <vegetka@gmail.com>
2018-07-22 12:20:40 +0100

    more on socket.io

--- Test/index.js
+++ Test/index.js
@@ -11,8 +11,16 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
+var buttonValue = 0;
+
 io.on('connection', function(socket){
     console.log('Connection to client estabilished');
+    io.emit('clicked message', buttonValue);
+    socket.on('clicked message', function(msg){
+        buttonValue = 1 - buttonValue;
+        io.emit('clicked message', buttonValue);
+        console.log('Received message from client!', msg);
+    });
     socket.on('disconnect', function(){
         console.log('Server has disconnected');
     });
